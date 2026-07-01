493b0457aceac14d9975cd6c3a54a790ffa982b1
BestWayGrow2025 <khaira.74735@gmail.com>
2026-07-02 00:53:17 +0530

    Fix alert message for missing core_initializer.js

--- system_admin_admin_creation_controller.js
+++ system_admin_admin_creation_controller.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
 function initPage() {
 
   if (typeof initCoreSystem !== "function") {
-    alert("core_system.js missing");
+    alert("core_initializer.js missing");
     throw new Error("STOP");
   }
 
