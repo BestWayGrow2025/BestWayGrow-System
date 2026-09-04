1d5079d9e845221d47cbde956ec181fcc3a44cbb
BestWayGrow2025 <khaira.74735@gmail.com>
2026-09-04 12:19:33 +0530

    Update alert message for missing core initializer

--- admin_withdrawal_authority.js
+++ admin_withdrawal_authority.js
@@ -31,7 +31,7 @@ function initPage() {
   if (typeof initCoreSystem === "function") {
     initCoreSystem();
   } else {
-    alert("core_system.js missing");
+   alert("core_initializer.js missing");
     throw new Error("STOP");
   }
 }
